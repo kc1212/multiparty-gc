3122ee614606c1211eb51cd6ce222fdb3a970d47
kc1212 <kc1212@users.noreply.github.com>
2025-03-02 15:44:07 +0100

    Implement BDOZ shares

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub trait GcPrf {
     fn run(&self, key: F128b, player: usize, gate: usize) -> F128b;
 }
 
+pub mod error;
 pub mod evaluator;
 pub mod garbler;
 pub mod prep;
--- src/sharing.rs
+++ src/sharing.rs
@@ -1,43 +1,184 @@
+use itertools::{Itertools, izip};
+use rand::{CryptoRng, Rng};
+use scuttlebutt::field::{FiniteField, IsSubFieldOf};
 use std::{collections::BTreeMap, ops::Add};
 
+use crate::error::GcError;
+
 #[derive(Clone)]
-pub struct AuthShare<T, U>
+pub struct AuthShare<ShareFF, MacFF>
 where
-    T: for<'a> Add<&'a T>,
-    U: for<'a> Add<&'a U>,
+    ShareFF: FiniteField,
+    MacFF: FiniteField,
 {
-    // The secret share <x>_i
-    pub share: T,
-    // The MAC values M_j^i[x], where value M_j^i[x] is under key j
-    pub mac_values: BTreeMap<usize, U>,
-    // The MAC Keys K_i^j[x], where value K_i^j[x] is under key i
-    pub mac_keys: BTreeMap<usize, U>,
+    /// The party ID that holds this share.
+    /// We use a small type to minimize message size.
+    pub party_id: u16,
+    /// The secret share x^i.
+    pub share: ShareFF,
+    /// The MACs M_j[x^i], where value M_j[x^i] is under key j.
+    /// The btree is keyed on j (the other party's index).
+    pub mac_values: BTreeMap<u16, MacFF>,
+    /// The MAC keys K_i[x^j], where value K_i[x^j] is under key i.
+    /// The btree is keyed on j (the other party's index).
+    pub mac_keys: BTreeMap<u16, MacFF>,
 }
 
-impl<T, U> Add<&AuthShare<T, U>> for AuthShare<T, U>
+impl<ShareFF, MacFF> Add<&AuthShare<ShareFF, MacFF>> for AuthShare<ShareFF, MacFF>
 where
-    T: for<'a> Add<&'a T>,
-    U: for<'a> Add<&'a U>,
+    ShareFF: FiniteField,
+    MacFF: FiniteField,
 {
-    type Output = AuthShare<T, U>;
+    type Output = AuthShare<ShareFF, MacFF>;
 
-    fn add(self, rhs: &AuthShare<T, U>) -> Self::Output {
-        todo!()
+    fn add(self, rhs: &AuthShare<ShareFF, MacFF>) -> Self::Output {
+        assert_eq!(self.party_id, rhs.party_id);
+        let new_share = self.share + rhs.share;
+        let new_mac_values = self
+            .mac_values
+            .keys()
+            .map(|j| (*j, self.mac_values[j] + rhs.mac_values[j]))
+            .collect::<BTreeMap<_, _>>();
+        let new_mac_keys = self
+            .mac_keys
+            .keys()
+            .map(|j| (*j, self.mac_keys[j] + rhs.mac_keys[j]))
+            .collect::<BTreeMap<_, _>>();
+        Self::Output {
+            party_id: self.party_id,
+            share: new_share,
+            mac_values: new_mac_values,
+            mac_keys: new_mac_keys,
+        }
     }
+}
+
+pub fn secret_share<ShareFF, MacFF, R>(
+    secret: ShareFF,
+    n: u16,
+    rng: &mut R,
+) -> (Vec<AuthShare<ShareFF, MacFF>>, Vec<MacFF>)
+where
+    ShareFF: FiniteField,
+    MacFF: FiniteField,
+    ShareFF: IsSubFieldOf<MacFF>,
+    R: Rng + CryptoRng,
+{
+    assert!(n > 0);
+    let mut shares = (0..n).map(|_| ShareFF::random(rng)).collect_vec();
+
+    // Sum trait is not implemented, so we use fold
+    let sum_shares = shares.iter().fold(ShareFF::ZERO, |acc, x| acc + *x);
+    // Correct the first share so that the sum of shares is the secret
+    shares[0] = shares[0] + secret - sum_shares;
+
+    // Generate the global Deltas
+    let deltas = (0..n).map(|_| MacFF::random(rng)).collect_vec();
+
+    // all the mac keys, ordered by party
+    let mut mac_keys = Vec::with_capacity((n * n) as usize);
+    for i in 0..n {
+        let mut party_i_keys = BTreeMap::new();
+        for j in 0..n {
+            if i != j {
+                party_i_keys.insert(j, MacFF::random(rng));
+            }
+        }
+        mac_keys.push(party_i_keys);
+    }
+
+    // all the macs, ordered by party
+    let mut mac_values = Vec::with_capacity((n * n) as usize);
+    for i in 0..n {
+        let mut party_i_macs = BTreeMap::new();
+        for j in 0..n {
+            if i != j {
+                // MAC_j[x^i] = x^i * Delta_j + K_j[x^i]
+                let m_j_i = shares[i as usize] * deltas[j as usize] + mac_keys[j as usize][&i];
+                party_i_macs.insert(j, m_j_i);
+            }
+        }
+        mac_values.push(party_i_macs);
+    }
+
+    // assemble everything together
+    (
+        izip!(0..n, shares, mac_values, mac_keys)
+            .map(|(i, share, mac, key)| AuthShare {
+                party_id: i,
+                share,
+                mac_values: mac,
+                mac_keys: key,
+            })
+            .collect(),
+        deltas,
+    )
+}
+
+pub fn verify_and_reconstruct<ShareFF, MacFF>(
+    n: u16,
+    shares: Vec<AuthShare<ShareFF, MacFF>>,
+    deltas: Vec<MacFF>,
+) -> Result<ShareFF, GcError>
+where
+    ShareFF: FiniteField,
+    MacFF: FiniteField,
+    ShareFF: IsSubFieldOf<MacFF>,
+{
+    // we need to check, for every shares x^i such that
+    // M_j[x^i] = x^i \Delta_j + K_j[x^i], for j in [n] \ i
+    // held by i                 held by j
+    for i in 0..n {
+        for j in 0..n {
+            if i != j {
+                let m_j_i = shares[i as usize].mac_values[&j];
+                let delta_j = deltas[j as usize];
+                let k_j_i = shares[j as usize].mac_keys[&i];
+                let share_i = shares[i as usize].share;
+
+                if m_j_i != share_i * delta_j + k_j_i {
+                    return Err(GcError::MacCheckFailure);
+                }
+            }
+        }
+    }
+
+    let result = shares.iter().fold(ShareFF::ZERO, |acc, x| acc + x.share);
+
+    Ok(result)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use scuttlebutt::ring::FiniteRing;
-    use swanky_field_binary::{F128b, F2};
-    
+    use scuttlebutt::{AesRng, ring::FiniteRing};
+    use swanky_field_binary::{F2, F128b};
+
     #[test]
-    fn test_sharing() {
-        let share: AuthShare<F2, F128b> = AuthShare {
-            share: F2::ZERO,
-            mac_keys: BTreeMap::new(),
-            mac_values: BTreeMap::new(),
-        };
+    fn test_sharing_sunshine() {
+        let mut rng = AesRng::new();
+        let n = 10u16;
+        let secret = F2::random(&mut rng);
+        let (shares, deltas) = secret_share::<_, F128b, _>(secret, n, &mut rng);
+        assert_eq!(secret, verify_and_reconstruct(n, shares, deltas).unwrap());
+    }
+
+    #[test]
+    fn test_sharing_failure() {
+        let mut rng = AesRng::new();
+        let n = 10u16;
+        let secret = F2::random(&mut rng);
+        {
+            // modify a share
+            let (mut shares, deltas) = secret_share::<_, F128b, _>(secret, n, &mut rng);
+            shares[0].share += F2::ONE;
+            verify_and_reconstruct(n, shares, deltas).unwrap_err();
+        }
+        {
+            // modify a delta
+            let (shares, mut deltas) = secret_share::<_, F128b, _>(secret, n, &mut rng);
+            deltas[0] += F128b::ONE;
+            verify_and_reconstruct(n, shares, deltas).unwrap_err();
+        }
     }
 }
